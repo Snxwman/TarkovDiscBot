189189a8ebd898069d2394e4d0a3a931cc6becc5
sillestudios <ellised8@gmail.com>
2026-01-17 15:08:27 -0500

    item dataclass helmet dataclass

---
+++ tarkovdiscbot/enums/equipment.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from dataclasses import dataclass
+from tarkovdiscbot.enums.items import ItemRarity
+
+class HelmetArmoredAreas(Enum):
+    HEAD_TOP = 'Head top'
+    NAPE = 'Nape'
+    EARS = 'Ears'
+
+class HelmetPenalties(Enum):
+    MOVE_SPEED = 'Movement speed'
+    TURN_SPEED = 'Turn Speed'
+    ERGO = 'Ergonomics'
+    SOUND_REDUCTION = 'Sound reduction'
+
+@dataclass
+
+class HelmetDataMixin:
+    rarity: ItemRarity
+    helemt_name: str
+    helmet_material: str
+    helmet_weight: int
+    helmet_armor_class: int
+    helmet_armored_areas: list[HelmetArmoredAreas]
+    helmet_durability: int
+    richochet_chance: str
+    helmet_penalties: list[HelmetPenalties]
+    blocks_headset: bool
+    blocks_eyewear: bool
+    blocks_facecover: bool
+
+class Helmet(HelmetDataMixin, Enum):
+
+    ALTYN = (ItemRarity.MYTHIC, 'Altyn bulletproof helmet', 'Armor steel', 2500, 5, 
+             [HelmetArmoredAreas.HEAD_TOP, HelmetArmoredAreas.NAPE, HelmetArmoredAreas.EARS],
+             81, 'High', [HelmetPenalties.MOVE_SPEED, HelmetPenalties.TURN_SPEED, HelmetPenalties.ERGO, HelmetPenalties.SOUND_REDUCTION],
+             True, False, False)
+
+
--- tarkovdiscbot/enums/items.py
+++ tarkovdiscbot/enums/items.py
@@ -1,4 +1,5 @@
 from enum import auto, Enum
+from dataclasses import dataclass
 
 class ItemRarity(Enum):
     COMMON = auto()
@@ -6,3 +7,14 @@ class ItemRarity(Enum):
     RARE = auto()
     LEGENDARY = auto()
     MYTHIC = auto()
+
+@dataclass
+class ItemDataMixin:
+    category = str
+    slot = str
+    name = str
+    rarity = ItemRarity
+    weight = int
+    sold_by = str
+    description = str
+    grid_size = int
